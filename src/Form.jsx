d6c7ea8f5e0ec9238a3ca91defd54b264d3beb58
Leo Adams <sam0941535901@gmail.com>
2025-12-27 23:24:30 +0200

    pt-12

--- src/Form.jsx
+++ src/Form.jsx
@@ -173,7 +173,7 @@ export default function Form() {
     <div
       className={`h-svh w-svw flex justify-center items-center bg text overflow-hidden`}
     >
-      <div className="relative bg-pop w-[90%] sm:max-w-lg max-h-[90%] rounded  shadow-lg py-6 px-4 flex flex-col gap-4 overflow-auto transition-all duration-300">
+      <div className="relative bg-pop w-[90%] sm:max-w-lg max-h-[90%] rounded  shadow-lg pt-12 pb-6 px-4 flex flex-col gap-4 overflow-auto transition-all duration-300">
         {/* Theme Toggle Button  */}
         <div className=" absolute top-2 right-2">
           <ThemeToggleButton />
